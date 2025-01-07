b6b83c306ef0abcc864d135af144c0a70386961b
June Hyck Lee <132459407+happylee8282@users.noreply.github.com>
2025-01-07 14:58:46 +0900

    Add files via upload

---
+++ build/robot_pose_getter/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/leejh104/ydlidar_ws/src/robot_pose_getter
+# Build directory: /home/leejh104/ydlidar_ws/build/robot_pose_getter
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
